c786fe8ee0fdcc036f68b1db4c8b6609299bc105
Cisco <francisco-hernandez@lambdastudents.com>
2020-10-14 17:33:25 -0700

    started axiosAuth

---
+++ medcab-front-end/src/api/axiosWithAuth.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+
+export const axiosWithAuth = () => {
+    const token = localStorage.getItem("authToken");
+    console.log("axiosWithAuth Token:", token);
+    return axios.create({
+        baseURL: "https://med-cab-bw.herokuapp.com/",
+        headers: {
+            Authorization: token
+        }
+    })
+}
